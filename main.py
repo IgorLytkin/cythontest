46e59b332b32d2a7563c100642ea863448c22402
Igor Lytkin <igor.lytkin.2020@gmail.com>
2021-11-15 06:14:56 +0700

    Start

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def test(x):
         y *= i
     return y
 
-cpdef int test(int x):
-    cdef int y = 1
-    cdef int i
-    for i in range(1, x+1):
-        y *= i
-    return y
+# cpdef int test(int x):
+#    cdef int y = 1
+#    cdef int i
+#    for i in range(1, x+1):
+#        y *= i
+#    return y
 
 
 def print_hi(name):
@@ -37,22 +37,17 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 
-number = 10
-
-start = time.time()
-run_python.test(number)
-end =  time.time()
-
-py_time = end - start
-print("Python time = {}".format(py_time))
-
-start = time.time()
-run_cython.test(number)
-end =  time.time()
-
-cy_time = end - start
-print("Cython time = {}".format(cy_time))
-
-print("Speedup = {}".format(py_time / cy_time))
+# number = 10
+# start = time.time()
+# run_python.test(number)
+# end =  time.time()
+# py_time = end - start
+# print("Python time = {}".format(py_time))
+# start = time.time()
+# run_cython.test(number)
+# end =  time.time()
+# cy_time = end - start
+# print("Cython time = {}".format(cy_time))
+# print("Speedup = {}".format(py_time / cy_time))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
